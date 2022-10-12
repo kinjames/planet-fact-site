a3d7b58b15720bba9a54f4b1604056a54dea7f35
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-10-12 23:15:41 +0000

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!')
+console.log('Hello, World!');
+
+let menuBtn = document.getElementById('menuBtn');
+let mobileMenu = document.getElementById('mobileMenu');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click',()=>{
+    if (mobileMenu.classList.contains('hide')){
+        mobileMenu.classList.remove('hide');
+        menuBtn.classList.add('burger');
+        body.classList.add('noscroll');
+    } else {
+        mobileMenu.classList.add('hide');
+        menuBtn.classList.remove('burger');
+        body.classList.remove('noscroll');
+    }
+})
